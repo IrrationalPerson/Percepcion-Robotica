bd774c5c15ab0ecf08801a587da0c61fb0cd7260
IrrationalPerson <99685883+IrrationalPerson@users.noreply.github.com>
2023-11-07 14:32:06 -0600

    Update pose_detection.py

--- pose_detection.py
+++ pose_detection.py
@@ -5,25 +5,18 @@ import cvzone
 import math
 import pyrealsense2 as rs
 
-# Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-pipeline.start(config)
-
-model = YOLO('yolov8n.pt')
-
-while True:
+def get_image(pipeline):
     frames = pipeline.wait_for_frames()
     depth_frame = frames.get_depth_frame()
     color_frame = frames.get_color_frame()
     
     depth_image = np.asanyarray(depth_frame.get_data())
     img = np.asanyarray(color_frame.get_data())
-    
-    results = model(img, stream=True, classes=0, conf=0.5)
+
+    return depth_image, img
+
+def object_pose(model, depth_image, img, conf, classes):
+    results = model(img, stream=True, conf=conf, classes=classes)
     for r in results:
         boxes = r.boxes
         for box in boxes:
@@ -31,7 +24,8 @@ while True:
             x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
             conf = math.ceil(box.conf[0]*100)/100
             cls = int(box.cls[0])
-            if cls == 0:
+            # for c in classes:
+            if cls == classes:
                 # Compute median depth within the bounding box
                 person_depth = depth_image[y1:y2, x1:x2]
                 median_distance = np.nanmedian(person_depth) / 1000.0  # Convert to meters and handle NaN values
@@ -40,7 +34,52 @@ while True:
                 cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
                 cvzone.putTextRect(img, f'Person Conf: {conf}', (max(0, x1), max(35, y1-20)), scale=1.5, thickness=1)
                 cvzone.putTextRect(img, f'Distance: {median_distance:.2f}m', (max(0, x1), max(35, y1-40)), scale=1.5, thickness=1)
+
+def object_detect(model, depth_image, img, conf, classes):
+    results = model(img, stream=True, conf=conf, classes=classes)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            conf = math.ceil(box.conf[0]*100)/100
+            cls = int(box.cls[0])
+            # for c in classes:
+            if cls == classes:
+                # Compute median depth within the bounding box
+                print("MANZANA sike es naranja")
+                return True
+    return False
+
+
     
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) == ord('q'):
-        break
+
+
+if __name__ == "__main__":
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+    pipeline.start(config)
+
+    model = YOLO('yolov8n.pt')
+
+    apple = 47
+    orange = 47
+    
+    pepinillo = False
+    
+    while not pepinillo:
+        depth_image, img = get_image(pipeline)
+        pepinillo = object_detect(model, depth_image, img, conf=0.05, classes=apple)
+            
+
+    while True:
+        
+        depth_image, img = get_image(pipeline)
+        object_pose(model, depth_image, img, conf=0.15, classes=orange)
+        
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
