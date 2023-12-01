11a0daa2c250fce10891b0256870122056f1e6c8
leodg021 <leodg_021@hotmail.com>
2023-11-30 23:05:26 -0600

    Added documentation comments.

--- pose_detection.py
+++ pose_detection.py
@@ -2,150 +2,156 @@ import numpy as np
 from ultralytics import YOLO
 import cv2
 import cvzone
-import math
 import pyrealsense2 as rs
 
 
-def get_image(pipeline, align):
-    frames = pipeline.wait_for_frames()
+def get_image(pipeline: rs.pipeline, align: rs.align) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Returns depth and color image from the camera.
+    """
+    frames = pipeline.wait_for_frames()     
     frames = align.process(frames)
-    depth_frame = frames.get_depth_frame()
-    # depth_frame = filtering_stage(depth_frame)
-
-    color_frame = frames.get_color_frame()
+    depth_frame = frames.get_depth_frame()  # Get depth frame.
+    color_frame = frames.get_color_frame()  # Get color frame.
     
+    # depth_frame = filtering_stage(depth_frame)    ## Use if you desire to filter the depth frame.
+
+    # Transform images into numpy arrays.
     depth_image = np.asanyarray(depth_frame.get_data())
     img = np.asanyarray(color_frame.get_data())
 
     return depth_image, img
 
+def filtering_stage(depth_frame: rs.depth_frame) -> rs.depth_frame:
+    """
+    Filters the depth frame in order to improve its quality. Use only if the code has been modified 
+    to take into consideration the change in scale due to the decimation filter.
+    """
 
-def object_pose(model, depth_image, img, confidence, classes, intr, coord):
-    results = model(img, stream=True, conf=confidence, classes=classes)
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
-            # obj_conf = math.ceil(box.conf[0]*100)/100
-            cls = int(box.cls[0])
-            # for c in classes:
-            if cls == classes:
-                # Compute median depth within the bounding box
-                person_depth = depth_image[y1:y2, x1:x2]
-                median_distance = np.nanmedian(person_depth) / 1000.0  # Convert to meters and handle NaN values
-
-                x = int(np.mean([x1, x2]))
-                y = int(np.mean([y1, y2]))
-
-                # pos = rs.rs2_deproject_pixel_to_point(intr, [x, y], median_distance)
-                pos = rs.rs2_deproject_pixel_to_point(intr, [x, y], median_distance)
-                x_coord = pos[0] - coord[0]
-                # y_coord = pos[1] - coord[1]
-                y_coord = pos[2] - coord[1]
-
-                angle = -np.pi/3.0  # Good angle: -np.pi/3.0
-
-                x_coord_rot = np.cos(angle) * x_coord - np.sin(angle) * y_coord
-                y_coord_rot = np.sin(angle) * x_coord + np.cos(angle) * y_coord
-
-                # x_coord = median_distance * (x - intr.ppx) / intr.fx - coord[0]
-                # y_coord = median_distance * (y - intr.ppy) / intr.fy - coord[1]
-                # z_coord = median_distance - coord[2]
-                
-                print(f'student coordinates = ({x_coord_rot:.2f}, {y_coord_rot:.2f})')
-                # print(median_distance)
-                # print(depth_image.shape)
-                # print(img.shape)
-
-
-                # x = int(np.mean([x1, x2]))
-                # y = int(np.mean([y1, y2]))
-                # median_distance = depth_image[int(y/5), int(x/5)] / 1000.0
-
-
-                print("x1; ", x1, " x2: ", x2, " y1: ", y1, " y2: ", y2)
-                if np.abs(y_coord_rot) < 0.90 and np.abs(x_coord_rot) < 1.70:
-                    colorRect = (0, 0, 255) # Red
-                else:
-                    colorRect = (0, 255, 0) # Green
-
-                cv2.rectangle(img, (x1, y1), (x2, y2), colorRect, 3)
-                # cvzone.putTextRect(img, f'Person Conf: {obj_conf}',
-                #                    (max(0, x1-40), max(35, y1-20)), scale=1.5, thickness=1)
-                # cvzone.putTextRect(img, f'Distance: {median_distance:.2f}m', (max(0, x1-40), max(35, y1-40)), scale=1.5, thickness=1)
-                cvzone.putTextRect(img, f'(x,y): ({x_coord_rot:.2f}, {y_coord_rot:.2f})',
-                                   (max(0, x1-40), max(35, y1-40)), scale=1.5, thickness=1,
-                                   colorR=colorRect)
-
-
-def object_detect(model, depth_image, img, confidence, classes, intr):
-    results = model(img, stream=True, conf=confidence, classes=classes)
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            # obj_conf = math.ceil(box.conf[0]*100)/100
-            cls = int(box.cls[0])
-            # for c in classes:
-            if cls == classes:
-                # Compute median depth within the bounding box
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
-                
-                person_depth = depth_image[y1:y2, x1:x2]
-                median_distance = np.nanmedian(person_depth) / 1000.0  # Convert to meters and handle NaN values
-
-                x = int(np.mean([x1, x2]))
-                y = int(np.mean([y1, y2]))
-
-                # pos = rs.rs2_deproject_pixel_to_point(intr, [x, y], median_distance)
-                pos = rs.rs2_deproject_pixel_to_point(intr, [x, y], median_distance)
-                x_coord = pos[0]
-                # y_coord = pos[1]
-                y_coord = pos[2]
-
-                # x_coord = median_distance * (x - intr.ppx) / intr.fx
-                # y_coord = median_distance * (y - intr.ppy) / intr.fy
-                # z_coord = median_distance
-                
-                print(f'origin coordinates = ({x_coord:.2f}, {y_coord:.2f})')
-                # print(x1, y1, x2, y2)
-                return [x_coord, y_coord]
-    return []
-
-# filters
-def filtering_stage(depth_frame):
-    #depth to disparity
+    # Depth to disparity and disparity to depth transforms.
     depth_to_disparity = rs.disparity_transform(True)
     disparity_to_depth = rs.disparity_transform(False)
 
-    # Decimation filter
+    # Decimation filter.
     decimation = rs.decimation_filter()
     decimation.set_option(rs.option.filter_magnitude, 5)
     depth_frame = decimation.process(depth_frame)
     depth_frame = depth_to_disparity.process(depth_frame)
 
-    # Spatial filter
+    # Spatial filter.
     spatial = rs.spatial_filter()
     spatial.set_option(rs.option.filter_magnitude, 5)
     spatial.set_option(rs.option.filter_smooth_alpha, 1)
     spatial.set_option(rs.option.filter_smooth_delta, 50)
     depth_frame = spatial.process(depth_frame)
 
-
-    # Temporal filter
+    # Temporal filter.
     temporal = rs.temporal_filter()
     depth_frame = temporal.process(depth_frame)
     depth_frame = disparity_to_depth.process(depth_frame)
 
-    # Hole Filling
+    # Hole filling filter.
     hole_filling = rs.hole_filling_filter()
     depth_frame = hole_filling.process(depth_frame)
 
     return depth_frame
 
+def object_detect(model: YOLO, depth_image: np.ndarray, img: np.ndarray, confidence: float,
+                  classes: list[int], intr: rs.intrinsics) -> list[float]:
+    """
+    Identify the object (STOP SIGN) that will serve as the reference marker for the position
+    estimation.
+    """
+    results = model(img, stream=True, conf=confidence, classes=classes) # Check the results from YOLO.
+
+    # Iterate over the results.
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+
+            cls = int(box.cls[0])   # If the class identified belonged to the STOP sign...
+            if cls == classes:
+
+                # Compute median depth within the bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
+                
+                # Convert to meters and handle NaN values
+                person_depth = depth_image[y1:y2, x1:x2]
+                median_distance = np.nanmedian(person_depth) / 1000.0  
+
+                x = int(np.mean([x1, x2]))
+                y = int(np.mean([y1, y2]))
+
+                # Deproject pixel into 3D point to obtain its coordinates.
+                pos = rs.rs2_deproject_pixel_to_point(intr, [x, y], median_distance)
+                x_coord = pos[0]    # X-coordinate
+                y_coord = pos[2]    # Y-coordinate
+                
+                print(f'origin coordinates = ({x_coord:.2f}, {y_coord:.2f})')
+                return [x_coord, y_coord]
+    return []
+
+def object_pose(model: YOLO, depth_image: np.ndarray, img: np.ndarray, confidence: float,
+                classes: list[int], intr: rs.intrinsics, coord: list[float]) -> list[float]:
+    """
+    Identify the persons in the space and compute their relative transformed coordinates according
+    to the plane axes of the machine to be monitored.
+    """
+    results = model(img, stream=True, conf=confidence, classes=classes) # Check the results from YOLO.
+    
+    # Iterate over the results.
+    for r in results:
+
+        boxes = r.boxes
+
+        for box in boxes:
+
+            cls = int(box.cls[0])   # If the class identified belonged to Person...
+            if cls == classes:
+                # Compute median depth within the bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
+
+                # Convert to meters and handle NaN values
+                person_depth = depth_image[y1:y2, x1:x2]
+                median_distance = np.nanmedian(person_depth) / 1000.0  
+
+                x = int(np.mean([x1, x2]))
+                y = int(np.mean([y1, y2]))
+
+                # Deproject pixel into 3D point to obtain its coordinates.
+                pos = rs.rs2_deproject_pixel_to_point(intr, [x, y], median_distance)
+                x_coord = pos[0] - coord[0]
+                y_coord = pos[2] - coord[1]
+
+                # Rotation angle to adjust the reference plane.
+                angle = np.pi/180.0*(-30.0)
+
+                # Rotate coordinates to reference plane.
+                x_coord_rot = np.cos(angle) * x_coord - np.sin(angle) * y_coord
+                y_coord_rot = np.sin(angle) * x_coord + np.cos(angle) * y_coord
+
+                
+                print(f'student coordinates = ({x_coord_rot:.2f}, {y_coord_rot:.2f})')
+                print("x1; ", x1, " x2: ", x2, " y1: ", y1, " y2: ", y2)
+
+                # Check if the person's coordinates stay within the permitted bounds.
+                if np.abs(y_coord_rot) < 0.90 and np.abs(x_coord_rot) < 1.70:
+                    colorRect = (0, 0, 255) # Red
+                else:
+                    colorRect = (0, 255, 0) # Green
+
+                # Draw the rectangle over the identified person and display their relative coordinates to the STOP sign.
+                cv2.rectangle(img, (x1, y1), (x2, y2), colorRect, 3)
+                cvzone.putTextRect(img, f'(x,y): ({x_coord_rot:.2f}, {y_coord_rot:.2f})',
+                                   (max(0, x1-40), max(35, y1-40)), scale=1.5, thickness=1,
+                                   colorR=colorRect)
+
 
 if __name__ == "__main__":
+
     # Configure depth and color streams
     pipeline = rs.pipeline()
     config = rs.config()
@@ -154,42 +160,35 @@ if __name__ == "__main__":
 
     profile = pipeline.start(config)
 
+    # Object to align color and depth frames.
     align_to = rs.stream.color
     align = rs.align(align_to)
 
-
+    # Obtain intrinsic parameters of the camera.
     intr = profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
 
+    # YOLO model.
     model = YOLO('yolov8n.pt')
 
-    origin = 11
-    student = 0
+    origin = 11     # Class id for STOP sign.
+    student = 0     # Class id for person.
     
     origin_coord = []
     
+    # Wait until the reference marker is identified and located.
     while not origin_coord:
         depth_image, img = get_image(pipeline, align)
-        origin_coord = object_detect(model, depth_image, img, confidence=0.05, classes=origin, intr=intr)
-
-    # x1 = detect_origin[0]
-    # y1 = detect_origin[1]
-    # x2 = detect_origin[2]
-    # y2 = detect_origin[3]
-
-    # x = int(np.mean(x1, x2))
-    # y = int(np.mean(y1, y2))
+        origin_coord = object_detect(model, depth_image, img, confidence=0.50, classes=origin, intr=intr)
    
-
+   # Continuously search for students in the image and display their coordinates.
     while True:
-        
         depth_image, img = get_image(pipeline, align)
-        object_pose(model, depth_image, img, confidence=0.15, classes=student, intr=intr, coord=origin_coord)
+        object_pose(model, depth_image, img, confidence=0.50, classes=student, intr=intr, coord=origin_coord)
         
         cv2.imshow("Image", img)
         if cv2.waitKey(1) == ord('q'):
             break
         
-
     pipeline.stop()
         
 
